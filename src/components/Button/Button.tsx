adb283b7f88c32f1929bc2c8d110c1efb854095d
Ridwan <afolabiridwan93@gmail.com>
2025-03-16 23:15:48 +0100

    correction on button hover effect and about section

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,20 +8,19 @@ interface propType {
 const Button: React.FC<propType> = ({ text, width, contStyle, bgType}) => {
     return (
         <div className={`group ${width} ${contStyle} h-[40px] relative duration-300`}>
-            <button className={`${bgType === "deepBlueBg"? "bg-deepBlue text-white mr-2 group-hover:bg-transparent  border-deepBlue" :
-                bgType === "transparentBg" ? "text-white border border-white":
-                bgType === "whiteBg" ? "border-white bg-white text-deepBlue group-hover:bg-transparent" :
-                bgType === "transparentBgBlueBorder" ? "text-deepBlue border border-deepBlue" : ""}
-                ${bgType === "deepBlueBg" ? "group-hover:text-deepBlue" : bgType === "transparentBg" ? 
-                "group-hover:text-deepBlue" : bgType === "whiteBg" ? "group-hover:text-white":
-                bgType === "transparentBgBlueBorder" ? "group-hover:text-white" :""} 
+            <button className={`${bgType === "deepBlueBg"? "bg-deepBlue text-white mr-2 group-hover:bg-[#1e2d5e]  border-deepBlue" :
+                bgType === "transparentBg" ? "text-white border border-white group-hover:bg-white":
+                bgType === "whiteBg" ? "border-white bg-white text-deepBlue group-hover:bg-[#edf2ff]" :
+                bgType === "transparentBgBlueBorder" ? "text-deepBlue border border-deepBlue group-hover:bg-[#edf2ff]" : ""}
+                ${bgType === "deepBlueBg" ? "group-hover:text-white" : bgType === "transparentBg" ? 
+                "group-hover:text-deepBlue" : bgType === "whiteBg" ? "group-hover:text-deepBlue" :""} 
                 cursor-pointer absolute top-0 w-[100%] flex items-center 
-                justify-center font-[600] h-[100%] border rounded-[7px] border-2 py-2`}>
+                justify-center font-[600] h-[100%] border rounded-[7px] border-2 py-2 duration-500`}>
                 {text}
             </button>
-            <div className={`rounded-[7px] ${bgType === "deepBlueBg" ? "bg-white" : bgType === "transparentBg" ? 
+            {/* <div className={`rounded-[7px] ${bgType === "deepBlueBg" ? "bg-white" : bgType === "transparentBg" ? 
                 "bg-white": bgType === "transparentBgBlueBorder" ? "bg-deepBlue" : ""} 
-                absolute top-0 left-0 w-0 h-[100%] z-[-1] duration-[1000ms] group-hover:w-[100%]`}></div>
+                absolute top-0 w-[100%] h-[100%] z-[-1] duration-[1000ms] opacity-0 group-hover:opacity-1`}></div> */}
         </div>
     )
 }
