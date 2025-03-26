cf46f97440b60e01429d4ca2a44e6b21726dc2bf
Ridwan <afolabiridwan93@gmail.com>
2025-03-26 10:31:45 +0100

    added registration page and some modification

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import { countries } from "../../provider/data";
 import ConversionLayout from "../../components/Layout/ConversionLayout";
-import { BadgeCheck, Crown } from "lucide-react";
 import { FaCrown, FaPuzzlePiece } from "react-icons/fa6";
-import { BiCrown } from "react-icons/bi";
 
 const RegistrationForm = () => {
     const [formData, setFormData] = useState({
