17da282ea80c21fdf0068f16a8e14078d2857f01
Ridwan <afolabiridwan93@gmail.com>
2025-03-26 21:32:54 +0100

    correction on accordions

--- src/pages/Home/AboutSection/AboutSection.tsx
+++ src/pages/Home/AboutSection/AboutSection.tsx
@@ -1,37 +1,38 @@
-import AccordionItem from "./AccordionItem"
-import aboutSectionImg from "/assets/image/gambardella.png"
-import brochureBg from "/assets/image/frame-bg.png"
-import Button from "../../../components/Button/Button"
-import { useState } from "react"
-import { aboutSectionData } from "../../../provider/data"
+import AccordionItem from "./AccordionItem";
+import aboutSectionImg from "/assets/image/gambardella.png";
+import brochureBg from "/assets/image/frame-bg.png";
+import Button from "../../../components/Button/Button";
+import { useState } from "react";
+import { aboutSectionData } from "../../../provider/data";
 
 const AboutSection = () => {
-  
 
-  const [openItemId, setOpenItemId] = useState<number | null>(null);
-
-  const accordionItemHandler = (id: number) => {
-    setOpenItemId(prevId => (prevId === id ? null : id));
-  };
+  const [openItemId, setOpenItemId] = useState<number>(aboutSectionData[0].id);
 
   return (
-    <section id="about-section" className='aboutSection py-20 lg:px-20 md:px-10  px-6 w-[100%]'>
+    <section id="about-section" className="aboutSection py-20 lg:px-20 md:px-10 px-6 w-[100%]">
       <p className="header">About</p>
-      <h1 className="text-[29px]  max-md:text-[24px] max-sm:text-[21px] font-[600] mt-6 text-center">The FEES Leadership Academy</h1>
-      <p className="text-gray text-[14px] max-sm:text-[12px] leading-[10px]">Discover the nitty gritty, the perks of the Academy</p>
+      <h1 className="text-[29px] max-md:text-[24px] max-sm:text-[21px] font-[600] mt-6 text-center">
+        The FEES Leadership Academy
+      </h1>
+      <p className="text-gray text-[14px] max-sm:text-[12px] leading-[10px]">
+        Discover the nitty gritty, the perks of the Academy
+      </p>
 
       <div className="md:mt-16 mt-8 flex justify-between max-md:flex-col-reverse outline-none select-none">
         <div className="md:w-[48%]">
-          <ul className="">
-            {aboutSectionData.map(item =>
+          <ul>
+            {aboutSectionData.map((item) => (
               <AccordionItem
+                key={item.id}
                 header={item.header}
                 text={item.text}
-                icon={item.icon} id={item.id}
+                icon={item.icon}
+                id={item.id}
                 isOpen={openItemId === item.id}
-                toggleItem={() => accordionItemHandler(item.id)}
-                key={item.id}
-              />)}
+                setOpenItem={setOpenItemId} 
+              />
+            ))}
           </ul>
         </div>
         <div className="md:w-[48%]">
@@ -39,21 +40,20 @@ const AboutSection = () => {
         </div>
       </div>
 
-      <div className={"xl:w-[1000px] xl:h-[130px] md:w-[80%] md:h-[100%] w-[100%] h-[100%]  md:py-8 relative flex max-md:flex-col justify-between items-center mt-12 "}>
+      <div className="xl:w-[1000px] xl:h-[130px] md:w-[80%] md:h-[100%] w-[100%] h-[100%] md:py-8 relative flex max-md:flex-col justify-between items-center mt-12">
         <img src={brochureBg} loading="lazy" alt="brochureBg" className="absolute w-[100%] h-[100%] object-cover object-left-top rounded-[10px]" />
         <div className="text-white md:w-[40%] w-[100%] z-4 md:pl-6 px-4 max-md:pt-6">
           <h1 className="md:text-[20px] text-[25px]">Explore Further</h1>
-
-          <p className="md:text-[13px] text-[15px]">For detailed information on the curvriculum,
-            key learning elements, and outcomes, download our
-            Academy Brochure below.</p>
+          <p className="md:text-[13px] text-[15px]">
+            For detailed information on the curriculum, key learning elements, and outcomes, download our Academy Brochure below.
+          </p>
         </div>
         <div className="max-md:w-[100%] md:pr-6 px-4 max-md:pb-6 z-6">
           <Button text="Download Brochure" bgType="whiteBg" style="sm:w-[200px] w-full max-md:mt-[30px]" />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutSection 
+export default AboutSection;
--- src/pages/Home/AboutSection/AccordionItem.tsx
+++ src/pages/Home/AboutSection/AccordionItem.tsx
@@ -1,35 +1,47 @@
-import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md"
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from "react-icons/md";
 
-interface accordionItemPropType {
-    header: string;
-    text?: string;
-    icon: string;
-    id: number;
-    isOpen: boolean;
-    toggleItem: () => void;
+interface AccordionItemPropType {
+  header: string;
+  text?: string;
+  icon: string;
+  id: number;
+  isOpen: boolean;
+  setOpenItem: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const AccordionItem: React.FC<accordionItemPropType> = ({ header, text, icon, id, isOpen, toggleItem }) => {
+const AccordionItem: React.FC<AccordionItemPropType> = ({ header, text, icon, id, isOpen, setOpenItem }) => {
+  return (
+    <li
+      className={`outline-none select-none border cursor-pointer border-[2px] ${
+        !isOpen ? "border-lightGray" : "border-blueOnAccordion"
+      } rounded-md p-4 ${(id === 2 || id === 3) && "mt-6"}`}
+      onMouseEnter={() => setOpenItem(id)} 
+    >
+      <div className="w-[100%] flex justify-between items-center">
+        <div className="flex items-start space-x-2">
+          <img src={icon} loading="lazy" alt="mapPinicon" className="w-[28px]" />
+          <span className="font-semibold text-[17px] max-sm:text-[15px]">{header}</span>
+        </div>
+        <div>
+          {!isOpen ? (
+            <MdOutlineKeyboardArrowDown className="text-[30px] text-gray transition-transform duration-300 ease-in-out" />
+          ) : (
+            <MdOutlineKeyboardArrowUp className="text-[30px] text-gray transition-transform duration-300 ease-in-out" />
+          )}
+        </div>
+      </div>
 
+      <div
+        className={`overflow-hidden transition-[max-height] duration-1000 ease-in-out ${
+          isOpen ? "max-h-[300px] opacity-100 mt-4" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="text-justify leading-relaxed text-gray text-[14px]">
+          <div dangerouslySetInnerHTML={{ __html: text || "" }} />
+        </div>
+      </div>
+    </li>
+  );
+};
 
-    return (
-            <li className={`outline-none select-none border cursor-pointer border-[2px] ${!isOpen ? "border-lightGray" : "border-blueOnAccordion"} 
-                rounded-md p-4 ${(id == 2 || id == 3) && "mt-6"}`} onClick={toggleItem}>
-                <div className="w-[100%] flex justify-between items-center">
-                    <div className="flex items-start space-x-2">
-                        <img src={icon} loading="lazy" alt="mapPinicon" className="w-[28px]" />
-                        <span className="font-semibold text-[17px] max-sm:text-[15px]">{header}</span>
-                    </div>
-                    <div>
-                        {!isOpen ? <i><MdOutlineKeyboardArrowDown className="text-[30px] text-gray" /></i> :
-                            <i><MdOutlineKeyboardArrowUp className="text-[30px] text-gray" /></i>}
-                    </div>
-                </div>
-                <div className={`${isOpen ? "block" : "hidden"} duration-700 text-justify leading-relaxed text-gray text-[14px] mt-6`}>
-                <div dangerouslySetInnerHTML={{ __html: text || "" }} />
-                </div>
-            </li>
-    )
-}
-
-export default AccordionItem
+export default AccordionItem;
