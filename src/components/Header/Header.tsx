851b55f866f10be7e50c8cf65b8b33cca687f8e8
Ridwan <afolabiridwan93@gmail.com>
2025-03-27 12:03:35 +0100

    corrections

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import logoWhite from "/assets/image/Logo Horizontal white 2.png";
 import logoColored from "/assets/image/Logo Horizontal color .png";
-import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdArrowRightAlt, MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { FaBars } from "react-icons/fa6";
 import MobileNav from "../MobileNav/MobileNav";
 import { useNavigate } from "react-router";
@@ -64,13 +64,13 @@ const Header = () => {
         <li className="navLink relative group flex items-center cursor-pointer">
           Services
           <i><MdOutlineKeyboardArrowDown className="text-[20px]" /></i>
-          <ul className="subMenu absolute top-[40px] left-0 w-[300px] translate-x-[-90px] py-6 space-y-4 text-deepBlue bg-white 
+          <ul className="subMenu absolute top-[40px] left-0 w-[300px] translate-x-[-90px] py-6  text-deepBlue bg-white 
           transition-all duration-1000 overflow-hidden rounded-md opacity-0 max-h-0 
           group-hover:opacity-100 group-hover:max-h-[300px] shadow-md">
-            <li>Skill Acquisition</li>
-            <li>Leadership Academy</li>
-            <li>Community Engagement</li>
-            <li>Talent Resourcing</li>
+            <li>Skill Acquisition <span ><MdArrowRightAlt/></span></li>
+            <li>Leadership Academy <span ><MdArrowRightAlt/></span></li>
+            <li>Community Engagement <span ><MdArrowRightAlt/></span></li>
+            <li>Talent Resourcing <span ><MdArrowRightAlt/></span></li>
           </ul>
         </li>
         <li className="navLink">Team</li>
