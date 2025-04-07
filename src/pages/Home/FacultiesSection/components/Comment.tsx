1037e1249602f464eb3ec5c3d6bb92cd5a1abe52
Ridwan <afolabiridwan93@gmail.com>
2025-04-07 15:40:48 +0100

    resolved some issues

--- src/pages/Home/FacultiesSection/components/Comment.tsx
+++ src/pages/Home/FacultiesSection/components/Comment.tsx
@@ -2,27 +2,29 @@ import { commentType } from "../../../../types/types";
 import headingIcon from "/public/assets/icons/Vector.png";
 
 
-const Comment = ({isExpanded, card}: commentType) => {
+const Comment = ({ isExpanded, card }: commentType) => {
     return (
         <div className={`card-info ${isExpanded ? "block" : "hidden"} md:block`}>
-            <p className="heading">BIO</p>
-            <p
-                className="bio"
-                dangerouslySetInnerHTML={{ __html: card.bio }}
-            />
-            <div>
-                <img src={headingIcon} loading="lazy" alt="headingIcon" />
-                <p className="mt-2 text-[14px]">
-                    {card.comment}
-                </p>
-            </div>
-            <div className="courses mt-6">
-                <p className="heading">Courses Taught</p>
-                <ul>
-                    {card.courses.map((course, idx) => (
-                        <li key={idx}>{course}</li>
-                    ))}
-                </ul>
+            <div className={`card-info-content`}>
+                <p className="heading">BIO</p>
+                <p
+                    className="bio"
+                    dangerouslySetInnerHTML={{ __html: card.bio }}
+                />
+                <div>
+                    <img src={headingIcon} loading="lazy" alt="headingIcon" />
+                    <p className="mt-2 text-[14px]">
+                        {card.comment}
+                    </p>
+                </div>
+                <div className="courses mt-6">
+                    <p className="heading">Courses Taught</p>
+                    <ul>
+                        {card.courses.map((course, idx) => (
+                            <li key={idx}>{course}</li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
